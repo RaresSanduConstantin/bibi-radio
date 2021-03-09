67acede6ad202afe1e4039df19360d7433538a5e
Rares Sandu <sandurares57@gmail.com>
2021-03-09 11:25:28 +0200

    filter style and favorite buttons styles

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 const Stations = ({ item }) => {
+	const [ favorite, setFavorite ] = useState(false);
+
+	const favoriteRadio = () => {
+		if (!favorite) {
+			setFavorite(true);
+		} else {
+			setFavorite(false);
+		}
+	};
+
 	return (
 		<div>
 			<div className="radioContainer">
 				<h2 className="radioTitle">{item.name}</h2>
+
+				<i
+					class={!favorite ? 'fas fa-heart fa-2x heartBg' : 'fas fa-heart fa-2x heartBgFav'}
+					onClick={favoriteRadio}
+				/>
+
 				<audio id="player" controls className="radioControls">
 					<source src={item.href} type="audio/ogg" className="radioSource" />
 				</audio>
