37afb59ae568ac9751157d3b24512f0c3b8a2170
Rares Sandu <59281487+RaresSanduConstantin@users.noreply.github.com>
2021-03-16 21:56:41 +0200

    tring to add favorite functionalities

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -1,59 +1,75 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
 
-const useAudio = url => {
-    const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
+  const toggle = () => setPlaying(!playing);
 
-    const toggle = () => setPlaying(!playing);
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing, audio]);
 
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-      },
-      [playing, audio]
-    );
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, [audio]);
 
-    useEffect(() => {
-      audio.addEventListener('ended', () => setPlaying(false));
-      return () => {
-        audio.removeEventListener('ended', () => setPlaying(false));
-      };
-    }, [audio]);
-
-    return [playing, toggle];
-  };
-
-
+  return [playing, toggle];
+};
 
 const Stations = ({ item }) => {
-	const [ favorite, setFavorite ] = useState(false);
-    const [playing, toggle] = useAudio(item.href);
-    console.log(playing)
-	const favoriteRadio = () => {
-		if (!favorite) {
-			setFavorite(true);
-		} else {
-			setFavorite(false);
-		}
-	};
+  const [favorite, setFavorite] = useState(false);
+  const [playing, toggle] = useAudio(item.href);
+  console.log(item);
+  const favoriteRadio = () => {
+    if (!favorite) {
+      setFavorite(true);
+      item.isFavorite = true;
+      localStorage.setItem("favoriteRadio", item.name);
+    } else {
+      setFavorite(false);
+      item.isFavorite = false;
+      localStorage.removeItem("favoriteRadio", item.name);
+    }
+  };
 
+  return (
+    <div>
+      <div className="radioContainer">
+        <div className="titleAndHeart" onClick={favoriteRadio}>
+          <h2 className="radioTitle">{item.name}</h2>
 
-
-	return (
-		<div>
-			<div className="radioContainer">
-				<div className="titleAndHeart" onClick={favoriteRadio}>
-					<h2 className="radioTitle">{item.name}</h2>
-
-					<i class={!favorite ? 'fas fa-heart fa-2x heartBg' : 'fas fa-heart fa-2x heartBgFav'} />
-				</div>
-				<div onClick={toggle} class={!playing ? "img-container" : "img-container play"}>
-					<img src={`${process.env.PUBLIC_URL}/static/${item.logo}.jpg`} alt="music-cover" id="cover" />
-				</div>
-                <button className='action-btn' onClick={toggle}>{playing ? <i class="fas fa-pause p"></i> : <i class="fas fa-play p"></i>}</button>
-			</div>
-		</div>
-	);
+          <i
+            class={
+              !favorite
+                ? "fas fa-heart fa-2x heartBg"
+                : "fas fa-heart fa-2x heartBgFav"
+            }
+          />
+        </div>
+        <div
+          onClick={toggle}
+          class={!playing ? "img-container" : "img-container play"}
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/${item.logo}.jpg`}
+            alt="music-cover"
+            id="cover"
+          />
+        </div>
+        <button className="action-btn" onClick={toggle}>
+          {playing ? (
+            <i class="fas fa-pause p"></i>
+          ) : (
+            <i class="fas fa-play p"></i>
+          )}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Stations;
